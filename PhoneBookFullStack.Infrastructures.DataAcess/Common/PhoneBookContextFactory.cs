e6e57ddb4b7de646e547b96e9d870d2b6c5d4b70
Saed Fathi <saed.fathi@gmail.com>
2019-11-08 07:51:36 +0330

    contextFactory

---
+++ PhoneBookFullStack.Infrastructures.DataAcess/Common/PhoneBookContextFactory.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace PhoneBookFullStack.Infrastructures.DataAcess.Common
+{
+    public class PhoneBookContextFactory :IDesignTimeDbContextFactory<PhoneBookContext>
+    {
+        public PhoneBookContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<PhoneBookContext>();
+            builder.UseSqlServer("Server=.;Database=PhoneBookDb;Integrated Security=True;MultipleActiveResultSets=true");
+
+            return new PhoneBookContext(builder.Options);
+        }
+    }
+}
